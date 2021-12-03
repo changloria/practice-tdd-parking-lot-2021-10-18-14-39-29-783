63a2e5bca85b6f333440882b620c9460cfd3716d
Gloria Chan <changl3@oocl.com>
2021-12-03 14:19:58 +0800

    [java-tdd-parking-lot][Gloria] - test: should_return_null_when_park_car_given_parking_lot_full_and_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -3,13 +3,19 @@ package com.parkinglot;
 import java.util.HashMap;
 
 public class ParkingLot {
+    private final int DEFAULT_CAPACITY = 10;
+    HashMap<Ticket, Car> ticketCarHashMap = new HashMap<>();
+    private int capacity;
+
+    public ParkingLot(int capacity){
+        this.capacity = capacity;
+    }
 
     public ParkingLot(){
-
+        this.capacity = DEFAULT_CAPACITY;
     }
 
     public Ticket parkCar(Car car) {
-        HashMap<Ticket, Car> ticketCarHashMap = new HashMap<>();
         Ticket ticket = new Ticket();
         ticketCarHashMap.put(ticket, car);
         return ticket;
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParkingLotTest {
     @Test
@@ -13,5 +14,16 @@ public class ParkingLotTest {
         Ticket ticket = parkingLot.parkCar(car);
         //then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_null_when_park_car_given_parking_lot_full_and_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        //when
+        Ticket ticket = parkingLot.parkCar(car);
+        //then
+        assertNull(ticket);
     }
 }
