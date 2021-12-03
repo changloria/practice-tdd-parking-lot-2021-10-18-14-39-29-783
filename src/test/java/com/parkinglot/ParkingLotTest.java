c82f499f94a58c80bebd79747ff297d9c85e58bb
Gloria Chan <changl3@oocl.com>
2021-12-03 15:37:13 +0800

    [java-tdd-parking-lot][Gloria] - test: should_throw_no_available_position_when_park_car_given_parking_lot_full_and_car + implement + edit should_return_no_available_position_exception_when_park_car_given_parking_lot_full_and_car

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -17,14 +17,14 @@ public class ParkingLotTest {
     }
 
     @Test
-    void should_return_null_when_park_car_given_parking_lot_full_and_car() {
+    void should_return_no_available_position_exception_when_park_car_given_parking_lot_full_and_car() {
         //given
         ParkingLot parkingLot = new ParkingLot(0);
         Car car = new Car();
         //when
-        Ticket ticket = parkingLot.parkCar(car);
+        NoAvailablePositionException noAvailablePositionException = assertThrows(NoAvailablePositionException.class, () -> {parkingLot.parkCar(car);});
         //then
-        assertNull(ticket);
+        assertEquals("No available position.", noAvailablePositionException.getMessage());
     }
 
     @Test
@@ -94,6 +94,6 @@ public class ParkingLotTest {
             parkingLot.parkCar(car);
         });
         //then
-        assertEquals("No available position", noAvailablePositionException.getMessage());
+        assertEquals("No available position.", noAvailablePositionException.getMessage());
     }
 }
