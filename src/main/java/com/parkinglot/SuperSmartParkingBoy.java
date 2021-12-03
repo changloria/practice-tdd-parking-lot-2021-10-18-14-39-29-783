c4b30eea3e1768e7ebc3cd68209e8bc8f01f9daa
Gloria Chan <changl3@oocl.com>
2021-12-03 19:57:29 +0800

    [java-tdd-parking-lot][Gloria] -implement testcase: should_park_first_parking_lot_when_park_car_given_super_smart_parking_boy_two_parking_lots_same_capacity

--- src/main/java/com/parkinglot/SuperSmartParkingBoy.java
+++ src/main/java/com/parkinglot/SuperSmartParkingBoy.java
@@ -6,4 +6,15 @@ public class SuperSmartParkingBoy extends SmartParkingBoy{
     public SuperSmartParkingBoy(ArrayList<ParkingLot> parkingLots) {
         super(parkingLots);
     }
+
+    @Override
+    public Ticket parkCar(Car car) {
+        ParkingLot availableParkingLot = getParkingLots().get(0);
+        for (int i = 1; i < getParkingLots().size(); i++) {
+            if (availableParkingLot.getAvailabilityRate() < getParkingLots().get(i).getAvailabilityRate()) {
+                availableParkingLot = getParkingLots().get(i);
+            }
+        }
+        return availableParkingLot.parkCar(car);
+    }
 }
